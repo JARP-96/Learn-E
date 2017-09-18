e8edb2f20b61e5722e965fc9841a1e646e4450fe
JARP-96 <jarp9665@gmail.com>
2017-09-18 15:21:27 -0500

    Proyecto para trabajar.

---
+++ Assets/Scenes/TitleScreen/Scripts/TitleScreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    public Texture backgroundTexture;
+    private void OnGUI()
+    {
+        // Display Background Texture
+        //GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+    }
+}

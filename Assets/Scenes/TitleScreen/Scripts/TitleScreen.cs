11801a8c25aa544f7426dc2fc969c2de8d0d4e87
JARP-96 <jarp9665@gmail.com>
2017-09-24 20:06:51 -0500

    Modificación TitleScreen
    Botón funcionando y pantalla con movimiento.

--- Assets/Scenes/TitleScreen/Scripts/TitleScreen.cs
+++ Assets/Scenes/TitleScreen/Scripts/TitleScreen.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TitleScreen : MonoBehaviour
 {
     public Texture backgroundTexture;
+    
     private void OnGUI()
     {
         // Display Background Texture
---
+++ Assets/Scenes/TitleScreen/Scripts/bttnNextScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bttnNextScene : MonoBehaviour {
+
+	public void changeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}

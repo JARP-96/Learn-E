9e6f29e80ebccfd39aee24b9f9b5fdc18ff57737
JARP-96 <jarp9665@gmail.com>
2017-09-29 23:40:29 -0500

    Listo para Login

--- Assets/Scenes/TitleScreen/Scripts/bttnNextScene.cs
+++ Assets/Scenes/TitleScreen/Scripts/bttnNextScene.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class bttnNextScene : MonoBehaviour {
 
-	public void changeScene(string sceneName)
+	public void changeScene(int sceneNumber)
     {
-        Application.LoadLevel(sceneName);
+        SceneManager.LoadScene(1);
+        //Application.LoadLevel(sceneNumber);
     }
 }
